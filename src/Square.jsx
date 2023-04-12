c6380a043a55948d3ecc5edfa7596916e658262e
River Hansen <riverbh01@gmail.com>
2023-04-12 07:43:02 -0600

    added a Square component

---
+++ src/Square.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Square = (props) => {
+    return (
+        <div className="square">
+            {props.propsVar}
+            </div>
+    )
+}
+
+export default Square
