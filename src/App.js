058fbd62260957f0a32bffa1f077d732e5feecdb
River Hansen <riverbh01@gmail.com>
2023-04-12 19:57:38 -0600

    created and managed state variables and passed them as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Square from './Square';
 
+
 function App() {
-  const person = "River"
+  const [squares, setSquares] = useState(['','','','','','','','',''])
+  const [player, setPlayer] = useState(true)
+
 
   return (
     <div className="App">
-      <Square propsVar={person}/>
+      <Square 
+        squares={squares}
+        setSquares={setSquares}
+        player={player}
+        setPlayer={setPlayer}
+      />
     </div>
   );
 }
